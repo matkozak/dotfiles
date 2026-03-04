094a831ee1b419f07e6af0a2c999bce0d6272a77
Mateusz Kozak <kozak.mateusz@gmail.com>
2026-03-04 18:31:08 +0100

    Add neovim QoL changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,6 +12,10 @@ vim.opt.signcolumn = "yes" -- always show gutter (git signs etc.)
 vim.opt.statuscolumn = "%s%=%l " -- signs first, right-aligned numbers second
 vim.opt.termguicolors = true -- 24-bit color
 vim.opt.cursorline = true -- highlight current line
+vim.opt.scrolloff = 10 -- keep cursor away from top/bottom edges
+vim.opt.sidescrolloff = 8 -- keep cursor away from left/right edges
+vim.opt.smoothscroll = true -- smooth <C-d>/<C-u> jumps
+vim.opt.wrap = false -- no line wrapping
 vim.opt.list = true -- show whitespace characters
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" } -- map whitespace chars
 
@@ -20,12 +24,14 @@ vim.opt.mouse = "a" -- mouse support
 vim.opt.clipboard = "unnamedplus" -- use OS clipboard
 vim.opt.undofile = true -- persist undo history across sessions
 vim.opt.breakindent = true -- wrapped lines continue visually indented (when wrap is on)
+vim.opt.linebreak = true -- wrap at word boundaries (when wrap is on)
 vim.opt.completeopt = { "menuone" } -- show menu even for single match, no auto-insert
 
 -- Indentation
 vim.opt.tabstop = 4 -- tab width
 vim.opt.shiftwidth = 4 -- indent width
 vim.opt.expandtab = true -- use spaces
+vim.opt.shiftround = true -- round indent to multiple of shiftwidth
 
 -- Search
 vim.opt.ignorecase = true -- case-insensitive by default
@@ -39,6 +45,8 @@ vim.opt.splitbelow = true -- :sp down
 
 -- Performance
 vim.opt.updatetime = 250 -- faster CursorHold and swap writes
+vim.opt.swapfile = false -- undofile is enough
+vim.opt.autowrite = true -- auto-save on buffer switch / :make
 
 -- Keymaps
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>") -- clear search highlight
@@ -49,6 +57,9 @@ vim.keymap.set("n", "<C-j>", "<cmd>wincmd j<cr>") -- C-j down
 vim.keymap.set("n", "<C-h>", "<cmd>wincmd h<cr>") -- C-h left
 vim.keymap.set("n", "<C-l>", "<cmd>wincmd l<cr>") -- C-l right
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float) -- show diagnostic float
+vim.keymap.set("v", "<", "<gv") -- stay in visual after dedent
+vim.keymap.set("v", ">", ">gv") -- stay in visual after indent
+vim.keymap.set("n", "<leader>q", "gqip") -- reflow paragraph to text width
 
 -- Diagnostics
 vim.diagnostic.config({
@@ -69,6 +80,25 @@ vim.diagnostic.config({
 	},
 	virtual_text = false, -- no inline text, signs + numhl are enough
 	underline = true,
+})
+
+-- Reload files changed outside nvim
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+	callback = function()
+		if vim.o.buftype ~= "nofile" then
+			vim.cmd("checktime")
+		end
+	end,
+})
+
+-- Soft wrap + spell for prose filetypes (gitcommit handled by ftplugin)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown", "text" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+		vim.opt_local.spelllang = "en,fr,pl"
+	end,
 })
 
 -- Bootstrap lazy.nvim
