089e3f14359792389287125b5d7cc7d4503000cd
Mateusz Kozak <kozak.mateusz@gmail.com>
2026-03-17 11:36:14 +0100

    Update lazy installation and setup
    Bring installation & setup in line with latest docs. Motivated mostly by
    lazy defaulting to outdated treesitter branch; hopefully we can remove
    it altogether when 0.12 is out.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -105,21 +105,39 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugins
 require("lazy").setup({
-	"HiPhish/rainbow-delimiters.nvim",
-	"ibhagwan/fzf-lua",
-	"lewis6991/gitsigns.nvim",
-	"neovim/nvim-lspconfig", -- server definitions
-	"nvim-treesitter/nvim-treesitter",
-}, {
+	checker = { enabled = false }, -- check for updates
+	defaults = {
+		lazy = false,
+		version = false, -- always use the latest git commit
+	},
 	performance = {
-		rtp = { reset = false }, -- don't mess with runtimepath
+		rtp = {
+			disabled_plugins = {}, -- if there's trouble, this is the way
+		},
+	},
+	spec = {
+		"HiPhish/rainbow-delimiters.nvim",
+		"ibhagwan/fzf-lua",
+		"lewis6991/gitsigns.nvim",
+		"neovim/nvim-lspconfig", -- server definitions
+		{ "nvim-treesitter/nvim-treesitter", lazy = false, build = ":TSUpdate" },
 	},
 })
 
